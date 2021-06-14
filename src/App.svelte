418c792690491e0ab73d102c255d5a8c7b0eff5e
Damien Buttler <damien@doublehops.com>
2021-06-14 15:05:55 +1000

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import CurrencyChart from "./CurrencyChart.svelte";
+	import queryString from "query-string";
+
+	export let name = 'eth';
+	export let queryParams;
+	$: queryParams = queryString.parse(location.search);
+</script>
+
+<main>
+	<h1>CoinHawk</h1>
+	<p>{queryParams.currency}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<CurrencyChart currency={queryParams.currency}></CurrencyChart>
---
+++ src/CurrencyChart.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Chart from 'svelte-frappe-charts';
+
+    export let currency;
+
+    let data = {
+        labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
+        datasets: [
+            {
+                values: [10, 12, 3, 9, 8, 15, 9]
+            }
+        ]
+    };
+</script>
+
+
+<style>
+    .chart-container {
+        margin: 0 auto;
+        width: 80%;
+    }
+</style>
+
+<div class="chart-container">
+    <Chart data={data} type="line"/>
+</div>
