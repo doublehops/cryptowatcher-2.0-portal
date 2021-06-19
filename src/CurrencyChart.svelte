d997624eade5fc6873219696aa97e02f7b9c02f0
Damien Buttler <damien@doublehops.com>
2021-06-19 14:20:09 +1000

    Using svelte chart.js

--- src/CurrencyChart.svelte
+++ src/CurrencyChart.svelte
@@ -1,16 +1,62 @@
 <script>
-    import Chart from 'svelte-frappe-charts';
+    import Line from "svelte-chartjs/src/Line.svelte"
 
     export let currency;
 
     let data = {
-        labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-        datasets: [
-            {
-                values: [10, 12, 3, 9, 8, 15, 9]
-            }
-        ]
+        datasets: [{
+            label: 'test 1',
+            data: [{
+                x: 1472194961,
+                y: 37
+            }, {
+                x: 1472281361,
+                y: 55
+            }, {
+                x: 1472367761,
+                y: 883
+            },
+            ]
+        }, {
+            label: 'test C',
+            data: [{
+                x: 1472410961,
+                y: 2
+            }, {
+                x: 1472324561,
+                y: 13
+            }, {
+                x: 1472238161,
+                y: 783
+            }]
+        }]
     };
+
+    let options = {
+        tooltips: {
+            callbacks: {
+                title(datasets) {
+                    var time = new Date(datasets[0].xLabel * 1000);
+                    return (time.getMonth() + 1) + '/' + time.getDate() + ' ' + time.getHours();
+                }
+            }
+        },
+        scales: {
+            xAxes: [
+                {
+                    type: 'linear',
+                    position: 'bottom',
+                    ticks: {
+                        callback(value) {
+                            var time = new Date(value * 1000);
+                            return (time.getMonth() + 1) + '/' + time.getDate() + ' ' + time.getHours();
+                        }
+                    }
+                }
+            ]
+        },
+        maintainAspectRatio: false,
+    }
 </script>
 
 
@@ -22,5 +68,10 @@
 </style>
 
 <div class="chart-container">
-    <Chart data={data} type="line"/>
+    <Line
+            data={data}
+            width={1100}
+            height={250}
+            options={options}
+    />
 </div>
