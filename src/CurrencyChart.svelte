e9439c81116f77cd05a7f569fb74455b8450f88a
Damien Buttler <damien@doublehops.com>
2021-06-19 16:17:18 +1000

    Chart mostly working with chart.js.

--- src/CurrencyChart.svelte
+++ src/CurrencyChart.svelte
@@ -6,76 +6,7 @@
     window.onload = function() {
         init();
     };
-    var d = new Date();
-    var t = d.getTime();
 
-    let data = {
-        datasets: [{
-            label: 'test 1',
-            data: [{
-                x: '2021-06-16 10:00:00',
-                y: 37
-            }, {
-                x: '2021-06-17 10:00:00',
-                y: 55
-            }, {
-                x: '2021-06-18 10:00:00',
-                y: 232
-            }, {
-                x: '2021-06-19 10:00:00',
-                y: 112
-            }]
-        }, {
-            label: 'test - '+ t,
-            data: [{
-                x: '2021-06-16 10:00:00',
-                y: 22
-            }, {
-                x: '2021-06-17 10:00:00',
-                y: 33
-            }, {
-                x: '2021-06-18 10:00:00',
-                y: 77
-            }, {
-                x: '2021-06-19 10:00:00',
-                y: 44
-            }]
-        }]
-    };
-
-    let options = {
-        // tooltips: {
-        //     callbacks: {
-        //         // title(datasets) {
-        //         //     var time = new Date(datasets[0].xLabel * 1000);
-        //         //     return (time.getMonth() + 1) + '/' + time.getDate() + ' ' + time.getHours();
-        //         // }
-        //     }
-        // },
-        // scales: {
-        //     xAxes: [
-        //         {
-        //             type: 'linear',
-        //             position: 'bottom',
-        //             // ticks: {
-        //             //     // callback(value) {
-        //             //     //     var time = new Date(value * 1000);
-        //             //     //     return (time.getMonth() + 1) + '/' + time.getDate() + ' ' + time.getHours();
-        //             //     // }
-        //             // }
-        //         }
-        //     ]
-        // },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Dollars'
-            }
-        },
-        maintainAspectRatio: false,
-    }
 
     function init() {
 
@@ -86,46 +17,70 @@
                 datasets: [{
                     label: 'test 1',
                     borderColor: 'rgba(255,0,0)',
-                    data: [{
-                        x: '2021-06-16 10:00:00',
-                        y: 23
-                    }, {
-                        x: '2021-06-17 10:00:00',
-                        y: 20
-                    }, {
-                        x: '2021-06-18 10:00:00',
-                        y: 21
-                    }, {
-                        x: '2021-06-19 10:00:00',
-                        y: 26
-                    }]
-                }, {
-                    label: 'test - '+ t,
-                    borderColor: 'rgba(0,255,0)',
                     background: false,
                     data: [{
                         x: '2021-06-16 10:00:00',
-                        y: 22
+                        // x: 1604082854,
+                        y: 23
                     }, {
                         x: '2021-06-17 10:00:00',
-                        y: 24
+                        // x: 1614082854,
+                        y: 20
                     }, {
                         x: '2021-06-18 10:00:00',
+                        // x: 1624082854,
+                        y: 21
+                    }, {
+                        x: '2021-06-19 10:00:00',
+                        // x: 1634082854,
+                        y: 26
+                    },
+                    ]
+                },
+                {
+                    label: 'test 2',
+                    borderColor: 'rgba(100,255,100)',
+                    background: false,
+                    data: [{
+                        x: '2021-06-16 10:00:00',
+                        // x: 1604082854,
+                        y: 21
+                    }, {
+                        x: '2021-06-17 10:00:00',
+                        // x: 1614082854,
+                        y: 23
+                    }, {
+                        x: '2021-06-18 10:00:00',
+                        // x: 1624082854,
                         y: 22
                     }, {
                         x: '2021-06-19 10:00:00',
-                        y: 25
-                    }]
-                }]
+                        // x: 1634082854,
+                        y: 27
+                    },]
+                }
+                ]
             },
             options: {
                 scales: {
                     y: {
                         beginAtZero: true,
-                    }
+                    },
+                    xAxes: [
+                        {
+                            type: 'time',
+                            position: 'bottom',
+                            time: {
+                                unit: 'day',
+                                unitStepSize: 1,
+                                displayFormats: {
+                                    'day': 'YYYY MMM DD'
+                                },
+                            },
+                        },
+                    ]
                 },
                 maintainAspectRatio: false,
-                backgroundColor: false,
             },
         });
     }
